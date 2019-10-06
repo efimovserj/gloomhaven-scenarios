903bbbef5a78bce14c1083f2ab11bf03b2c2a915
Sergey Efimov <s.efimov@it.ecommpay.com>
2019-10-07 00:30:04 +0300

    Add images for map

---
+++ src/utils/world-map.js
@@ -0,0 +1,572 @@
+export const worldMap =[
+  {
+    "id": 1,
+    "name": "black barrow",
+    "image": "world-map/1.png",
+    "xws": "scenario1"
+  },
+  {
+    "id": 2,
+    "name": "barrow lair",
+    "image": "world-map/2.png",
+    "xws": "scenario2"
+  },
+  {
+    "id": 3,
+    "name": "inox encampment",
+    "image": "world-map/3.png",
+    "xws": "scenario3"
+  },
+  {
+    "id": 4,
+    "name": "crypt of the damned",
+    "image": "world-map/4.png",
+    "xws": "scenario4"
+  },
+  {
+    "id": 5,
+    "name": "ruinous crypt",
+    "image": "world-map/5.png",
+    "xws": "scenario5"
+  },
+  {
+    "id": 6,
+    "name": "decaying crypt",
+    "image": "world-map/6.png",
+    "xws": "scenario6"
+  },
+  {
+    "id": 7,
+    "name": "vibrant grotto",
+    "image": "world-map/7.png",
+    "xws": "scenario7"
+  },
+  {
+    "id": 8,
+    "name": "gloomhaven warehouse",
+    "image": "world-map/8.png",
+    "xws": "scenario8"
+  },
+  {
+    "id": 9,
+    "name": "diamond mine",
+    "image": "world-map/9.png",
+    "xws": "scenario9"
+  },
+  {
+    "id": 10,
+    "name": "plane of elemental power",
+    "image": "world-map/10.png",
+    "xws": "scenario10"
+  },
+  {
+    "id": 11,
+    "name": "gloomhaven square a",
+    "image": "world-map/11-12.png",
+    "xws": "scenario11"
+  },
+  {
+    "id": 12,
+    "name": "gloomhaven square b",
+    "image": "world-map/11-12.png",
+    "xws": "scenario12"
+  },
+  {
+    "id": 13,
+    "name": "temple of the seer",
+    "image": "world-map/13.png",
+    "xws": "scenario13"
+  },
+  {
+    "id": 14,
+    "name": "frozen hollow",
+    "image": "world-map/14.png",
+    "xws": "scenario14"
+  },
+  {
+    "id": 15,
+    "name": "shrine of strength",
+    "image": "world-map/15.png",
+    "xws": "scenario15"
+  },
+  {
+    "id": 16,
+    "name": "mountain pass",
+    "image": "world-map/16.png",
+    "xws": "scenario16"
+  },
+  {
+    "id": 17,
+    "name": "lost island",
+    "image": "world-map/17.png",
+    "xws": "scenario17"
+  },
+  {
+    "id": 18,
+    "name": "abandoned sewers",
+    "image": "world-map/18.png",
+    "xws": "scenario18"
+  },
+  {
+    "id": 19,
+    "name": "forgotten crypt",
+    "image": "world-map/19.png",
+    "xws": "scenario19"
+  },
+  {
+    "id": 20,
+    "name": "necromancer's sanctum",
+    "image": "world-map/20.png",
+    "xws": "scenario20"
+  },
+  {
+    "id": 21,
+    "name": "infernal throne",
+    "image": "world-map/21.png",
+    "xws": "scenario21"
+  },
+  {
+    "id": 22,
+    "name": "temple of the elements",
+    "image": "world-map/22.png",
+    "xws": "scenario22"
+  },
+  {
+    "id": 23,
+    "name": "deep ruins",
+    "image": "world-map/23.png",
+    "xws": "scenario23"
+  },
+  {
+    "id": 24,
+    "name": "echo chamber",
+    "image": "world-map/24.png",
+    "xws": "scenario24"
+  },
+  {
+    "id": 25,
+    "name": "icecrag ascent",
+    "image": "world-map/25.png",
+    "xws": "scenario25"
+  },
+  {
+    "id": 26,
+    "name": "ancient cistern",
+    "image": "world-map/26.png",
+    "xws": "scenario26"
+  },
+  {
+    "id": 27,
+    "name": "ruinous rift",
+    "image": "world-map/27.png",
+    "xws": "scenario27"
+  },
+  {
+    "id": 28,
+    "name": "outer ritual chamber",
+    "image": "world-map/28.png",
+    "xws": "scenario28"
+  },
+  {
+    "id": 29,
+    "name": "sanctuary of gloom",
+    "image": "world-map/29.png",
+    "xws": "scenario29"
+  },
+  {
+    "id": 30,
+    "name": "shrine of the depths",
+    "image": "world-map/30.png",
+    "xws": "scenario30"
+  },
+  {
+    "id": 31,
+    "name": "plane of night",
+    "image": "world-map/31.png",
+    "xws": "scenario31"
+  },
+  {
+    "id": 32,
+    "name": "decrepit wood",
+    "image": "world-map/32.png",
+    "xws": "scenario32"
+  },
+  {
+    "id": 33,
+    "name": "savvas armory",
+    "image": "world-map/33.png",
+    "xws": "scenario33"
+  },
+  {
+    "id": 34,
+    "name": "scorched summit",
+    "image": "world-map/34.png",
+    "xws": "scenario34"
+  },
+  {
+    "id": 35,
+    "name": "gloomhaven battlements a",
+    "image": "world-map/35-36.png",
+    "xws": "scenario35"
+  },
+  {
+    "id": 36,
+    "name": "gloomhaven battlements b",
+    "image": "world-map/35-36.png",
+    "xws": "scenario36"
+  },
+  {
+    "id": 37,
+    "name": "doom trench",
+    "image": "world-map/37.png",
+    "xws": "scenario37"
+  },
+  {
+    "id": 38,
+    "name": "slave pens",
+    "image": "world-map/38.png",
+    "xws": "scenario38"
+  },
+  {
+    "id": 39,
+    "name": "treacherous divide",
+    "image": "world-map/39.png",
+    "xws": "scenario39"
+  },
+  {
+    "id": 40,
+    "name": "ancient defense network",
+    "image": "world-map/40.png",
+    "xws": "scenario40"
+  },
+  {
+    "id": 41,
+    "name": "timeworn tomb",
+    "image": "world-map/41.png",
+    "xws": "scenario41"
+  },
+  {
+    "id": 42,
+    "name": "realm of the voice",
+    "image": "world-map/42.png",
+    "xws": "scenario42"
+  },
+  {
+    "id": 43,
+    "name": "drake nest",
+    "image": "world-map/43.png",
+    "xws": "scenario43"
+  },
+  {
+    "id": 44,
+    "name": "tribal assault",
+    "image": "world-map/44.png",
+    "xws": "scenario44"
+  },
+  {
+    "id": 45,
+    "name": "rebel swamp",
+    "image": "world-map/45.png",
+    "xws": "scenario45"
+  },
+  {
+    "id": 46,
+    "name": "nightmare peak",
+    "image": "world-map/46.png",
+    "xws": "scenario46"
+  },
+  {
+    "id": 47,
+    "name": "lair of the unseeing eye",
+    "image": "world-map/47.png",
+    "xws": "scenario47"
+  },
+  {
+    "id": 48,
+    "name": "shadow weald",
+    "image": "world-map/48.png",
+    "xws": "scenario48"
+  },
+  {
+    "id": 49,
+    "name": "rebel's stand",
+    "image": "world-map/49.png",
+    "xws": "scenario49"
+  },
+  {
+    "id": 50,
+    "name": "ghost fortress",
+    "image": "world-map/50.png",
+    "xws": "scenario50"
+  },
+  {
+    "id": 51,
+    "name": "the void",
+    "image": "world-map/51.png",
+    "xws": "scenario51"
+  },
+  {
+    "id": 52,
+    "name": "noxious cellar",
+    "image": "world-map/52.png",
+    "xws": "scenario52"
+  },
+  {
+    "id": 53,
+    "name": "crypt basement",
+    "image": "world-map/53.png",
+    "xws": "scenario53"
+  },
+  {
+    "id": 54,
+    "name": "palace of ice",
+    "image": "world-map/54.png",
+    "xws": "scenario54"
+  },
+  {
+    "id": 55,
+    "name": "foggy thicket",
+    "image": "world-map/55.png",
+    "xws": "scenario55"
+  },
+  {
+    "id": 56,
+    "name": "bandit's wood",
+    "image": "world-map/56.png",
+    "xws": "scenario56"
+  },
+  {
+    "id": 57,
+    "name": "investigation",
+    "image": "world-map/57.png",
+    "xws": "scenario57"
+  },
+  {
+    "id": 58,
+    "name": "bloody shack",
+    "image": "world-map/58.png",
+    "xws": "scenario58"
+  },
+  {
+    "id": 59,
+    "name": "forgotten grove",
+    "image": "world-map/59.png",
+    "xws": "scenario59"
+  },
+  {
+    "id": 60,
+    "name": "alchemy lab",
+    "image": "world-map/60.png",
+    "xws": "scenario60"
+  },
+  {
+    "id": 61,
+    "name": "fading lighthouse",
+    "image": "world-map/61.png",
+    "xws": "scenario61"
+  },
+  {
+    "id": 62,
+    "name": "pit of souls",
+    "image": "world-map/62.png",
+    "xws": "scenario62"
+  },
+  {
+    "id": 63,
+    "name": "magma pit",
+    "image": "world-map/63.png",
+    "xws": "scenario63"
+  },
+  {
+    "id": 64,
+    "name": "underwater lagoon",
+    "image": "world-map/64.png",
+    "xws": "scenario64"
+  },
+  {
+    "id": 65,
+    "name": "sulfur mine",
+    "image": "world-map/65.png",
+    "xws": "scenario65"
+  },
+  {
+    "id": 66,
+    "name": "clockwork cove",
+    "image": "world-map/66.png",
+    "xws": "scenario66"
+  },
+  {
+    "id": 67,
+    "name": "arcane library",
+    "image": "world-map/67.png",
+    "xws": "scenario67"
+  },
+  {
+    "id": 68,
+    "name": "toxic moor",
+    "image": "world-map/68.png",
+    "xws": "scenario68"
+  },
+  {
+    "id": 69,
+    "name": "well of the unfortunate",
+    "image": "world-map/69.png",
+    "xws": "scenario69"
+  },
+  {
+    "id": 70,
+    "name": "chained isle",
+    "image": "world-map/70.png",
+    "xws": "scenario70"
+  },
+  {
+    "id": 71,
+    "name": "windswept highlands",
+    "image": "world-map/71.png",
+    "xws": "scenario71"
+  },
+  {
+    "id": 72,
+    "name": "oozing grove",
+    "image": "world-map/72.png",
+    "xws": "scenario72"
+  },
+  {
+    "id": 73,
+    "name": "rockslide ridge",
+    "image": "world-map/73.png",
+    "xws": "scenario73"
+  },
+  {
+    "id": 74,
+    "name": "merchant ship",
+    "image": "world-map/74.png",
+    "xws": "scenario74"
+  },
+  {
+    "id": 75,
+    "name": "overgrown graveyard",
+    "image": "world-map/75.png",
+    "xws": "scenario75"
+  },
+  {
+    "id": 76,
+    "name": "harrower hive",
+    "image": "world-map/76.png",
+    "xws": "scenario76"
+  },
+  {
+    "id": 77,
+    "name": "vault of secrets",
+    "image": "world-map/77.png",
+    "xws": "scenario77"
+  },
+  {
+    "id": 78,
+    "name": "sacrifice pit",
+    "image": "world-map/78.png",
+    "xws": "scenario78"
+  },
+  {
+    "id": 79,
+    "name": "lost temple",
+    "image": "world-map/79.png",
+    "xws": "scenario79"
+  },
+  {
+    "id": 80,
+    "name": "vigil keep",
+    "image": "world-map/80.png",
+    "xws": "scenario80"
+  },
+  {
+    "id": 81,
+    "name": "temple of the eclipse",
+    "image": "world-map/81.png",
+    "xws": "scenario81"
+  },
+  {
+    "id": 82,
+    "name": "burning mountain",
+    "image": "world-map/82.png",
+    "xws": "scenario82"
+  },
+  {
+    "id": 83,
+    "name": "shadows within",
+    "image": "world-map/83.png",
+    "xws": "scenario83"
+  },
+  {
+    "id": 84,
+    "name": "crystalline cave",
+    "image": "world-map/84.png",
+    "xws": "scenario84"
+  },
+  {
+    "id": 85,
+    "name": "sun temple",
+    "image": "world-map/85.png",
+    "xws": "scenario85"
+  },
+  {
+    "id": 86,
+    "name": "harried village",
+    "image": "world-map/86.png",
+    "xws": "scenario86"
+  },
+  {
+    "id": 87,
+    "name": "corrupted cove",
+    "image": "world-map/87.png",
+    "xws": "scenario87"
+  },
+  {
+    "id": 88,
+    "name": "plane of water",
+    "image": "world-map/88.png",
+    "xws": "scenario88"
+  },
+  {
+    "id": 89,
+    "name": "syndicate hideout",
+    "image": "world-map/89.png",
+    "xws": "scenario89"
+  },
+  {
+    "id": 90,
+    "name": "demonic rift",
+    "image": "world-map/90.png",
+    "xws": "scenario90"
+  },
+  {
+    "id": 91,
+    "name": "wild melee",
+    "image": "world-map/91.png",
+    "xws": "scenario91"
+  },
+  {
+    "id": 92,
+    "name": "back alley brawl",
+    "image": "world-map/92.png",
+    "xws": "scenario92"
+  },
+  {
+    "id": 93,
+    "name": "sunken vessel",
+    "image": "world-map/93.png",
+    "xws": "scenario93"
+  },
+  {
+    "id": 94,
+    "name": "vermling nest",
+    "image": "world-map/94.png",
+    "xws": "scenario94"
+  },
+  {
+    "id": 95,
+    "name": "payment due",
+    "image": "world-map/95.png",
+    "xws": "scenario95"
+  },
+];
